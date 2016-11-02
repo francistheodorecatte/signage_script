bde6c99e76fb6a9cd5bd337c53cc993ea4e5b369
phineasthecat <hellomunkey333@gmail.com>
2016-11-01 23:35:16 -0400

    bug fixin'

--- signage_script.sh
+++ signage_script.sh
@@ -5,13 +5,13 @@
 ##requires omxplayer,pqiv and cifs-utils to work.
 
 ##VARIABLES
-smbAddress={smb_address} //eg. 192.168.111.10
-smbFilepath={filepath} //eg. /signage
-smbUser={smb_user} 
-smbPass={smb_pass}
-smbMountPoint=/media/smb
-localFolder=/media/local
-signLogo={sign}.png
+smbAddress='{smb_address}' ##update!
+smbFilepath='{filepath}' ##update!
+smbUser='{smb_user}' ##update!
+smbPass='{smb_pass}' ##update!
+smbMountPoint='/media/smb'
+localFolder='/media/local'
+signLogo='{sign}.png' ##update!
 signName=$HOSTNAME
 smbDisk="//${smbAddress}/${smbFilepath} $mountPoint cifs -o username=$smb_user,password=$smb_pass,user 0 0"
 ramDiskMountPoint='/media/ram0'
@@ -44,14 +44,14 @@ function log() {
 	#(if it works)
 
 	currentTime=$(date '+%d/%m/%Y %H:%M:%S'); ##gets current day, month, year, hour, minute and second
-	echo $currentTime >> ${local_folder}/${sign_name}_log.txt 
+	echo $currentTime >> ${local_folder}/${sign_name}_log.txt
 	echo $1 &> ${local_folder}/${sign_name}_log.txt #pipes the redirected stdout/stderr to the log
 	#NOT SURE IF THIS IS GONNA WORK LOL
 	//abusing pipes and redirects like this is something I've never tried
 
 	sed -i -e '$a\' ${local_folder}/$sign_name}log.txt ##adding a new line to the log
-	
-} 
+
+}
 
 ##MAIN PROGRAM
 if ps -p $scriptPID > /dev/null ##check if script is already running
@@ -69,11 +69,12 @@ else
 	mkdir $ramDiskMountPoint
 	sed -i -e '$a\' /etc/fstab && sed -i -e '$ramDisk' /etc/fstab ##copy new ramdisk mounting lines to fstab
 	mount -a
-	if [ "$(ls -A ${ramDiskMountPoint})" ]; then
+	if [ "$(ls -A ${ramDiskMountPoint})" ]; then ##check if the ram disk is mounted
 		echo "ramdisk failed to mount!" | log
+		exit
 	else
 		echo "ramdisk mounted." | log
-	fi	
+	fi
 fi
 
 if grep -q
@@ -81,8 +82,10 @@ if grep -q
 if [ "$(ls -A ${mount_point})" ]; then ##check if mount point is empty
 	mkdir $smbMountPoint
 	sed -i -e '$a\' /etc/fstab && sed -i -e '$smbDisk' /etc/fstab ##copy new smb mounting lines to fstab
+	mount -a
 	if [ "$(ls -A ${smbMountPoint})" ]; then
 		echo "SMB failed to mount!" | log
+		exit
 	else
 		echo "SMB mounted." | log
 	fi
@@ -91,17 +94,34 @@ fi
 echo $BASHPID >> /tmp/signage_script.pid ##write out this script instance's PID to a file
 
 while true
-	remoteFileTime='stat -c %Y "${smbMountPoint}/${sign_name}.mp4"'
-	ramFileCopy
-	wait ${!}
+	remoteFileTime='stat -c %Y "${smbMountPoint}/${sign_name}.mp4"' ##update the remote file MTIME every time the loop restarts
+	if [ "$(ls -A ${ramDiskMountPoint}/${signName}.mp4)" ]; then ##check if the local file has been copied to RAM
+		ramFileCopy
+		wait ${!}
+	fi
+
+	if [ "$(ls -A  ${ramDiskMountPoint}/${signName}.mp4)" ]; then ##check if the video file is in RAM
+		if "$(ls -A ${smbMountPoint/${signLogo})" ] then
+			echo "No video or logo to display found!" | log ##complain that we have nothing to do
+		else
+			echo "No video to display found!" | log
+			pqiv --fullscreen ${smbMountPoint}/${signLogo} | log ##display the logo while we wait for the video to appear
+		fi
+	fi
+
 	if remoteFileTime>=localFileTime
 		remoteFileCopy
 		wait ${!}
 		killall omxplayer
+		killall pqiv
 		pqiv --fullscreen ${smbMountPoint}/${signLogo} & | log ##display fullscreen image while the player refreshes
 		ramFileCopy
 		wait ${!}
 		videoPlayer
+	else
+		killall omxplayer
+		videoPlayer
 	fi
+
 	sleep 1m ##sleep the infinite loop for one minute
 done
