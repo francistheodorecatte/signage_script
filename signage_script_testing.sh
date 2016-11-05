b8bc7290e24f5841dcd5bbee19311b373774c588
phineasthecat <hellomunkey333@gmail.com>
2016-11-05 15:16:24 -0400

    I made a typo
    oops

--- signage_script_testing.sh
+++ signage_script_testing.sh
@@ -105,7 +105,7 @@ if [ "$(ls -A ${ramDiskMountPoint}/${signName}.mp4)" ]; then
 fi
 
 while true; do
-	remoteMD5Hash=$(md5sum -b ${smbMountPoint}/${signName}.mp4s) ##update the remote file's MD5 hash every time the loop restarts
+	remoteMD5Hash=$(md5sum -b ${smbMountPoint}/${signName}.mp4) ##update the remote file's MD5 hash every time the loop restarts
 	if [ "$(ls -A ${localFolder}/${signName}.mp4)" ]; then ##do some sanity checking on the local file time
 		localFileTime=$(md5sum -b ${localFolder}/${signName}.mp4)
 	else
