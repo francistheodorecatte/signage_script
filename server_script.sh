13c76a28ce777b4e3c0595ec27d1ad305fe2a3b5
phineasthecat <hellomunkey333@gmail.com>
2016-11-06 14:39:36 -0500

    Starting work on the server-side of this setup. Don't expect anything from it

---
+++ server_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##Server Script companion for the Rasberry Pi digital signage script.
+##Version .01 by Joseph Keller, 2016
+##Pulls videos for display down from Google Drive, automatically renames them, and puts them in an SMB file share.
+##Requires a full Rasbian installation and rclone to work
+
+
